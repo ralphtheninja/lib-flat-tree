9e347cb393a8897003cb23e3cdde95080ef58fd7
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-04-05 17:45:57 +0200

    add TEST macro and make tests purdier :art:

--- test.c
+++ test.c
@@ -2,48 +2,53 @@
 #include <stdlib.h>
 #include "libflattree.h"
 
-#define assert(A, M, ...)                      \
-  if (!(A)) {                                  \
-    fprintf(stderr,                            \
-            "[FAIL] (%s:%d) " M "\n",          \
-            __FILE__, __LINE__,                \
-            ##__VA_ARGS__);                    \
-    exit(1);                                   \
-  } else {                                     \
-    printf("[OK] " M "\n");                    \
+int TEST_RESULT = 0;
+
+#define TEST(fn)                               \
+  printf("# Testing " #fn "\n");               \
+  fn();
+
+#define ASSERT(A)                                         \
+  if (!(A)) {                                             \
+    printf(" [FAIL] " #A "(%s:%d)\n",                     \
+           __FILE__, __LINE__);                           \
+    TEST_RESULT = 1;                                      \
+  } else {                                                \
+    printf(" [OK] " #A "\n");                             \
   }
+
 
 void test_index()
 {
-  assert(lft_index(0, 0) == 0, "index");
-  assert(lft_index(0, 1) == 2, "index");
-  assert(lft_index(0, 2) == 4, "index");
-  assert(lft_index(1, 0) == 1, "index");
-  assert(lft_index(1, 1) == 5, "index");
-  assert(lft_index(2, 0) == 3, "index");
-  assert(lft_index(2, 1) == 11, "index");
-  assert(lft_index(3, 0) == 7, "index");
-  assert(lft_index(3, 1) == 23, "index");
+  ASSERT(lft_index(0, 0) == 0);
+  ASSERT(lft_index(0, 1) == 2);
+  ASSERT(lft_index(0, 2) == 4);
+  ASSERT(lft_index(1, 0) == 1);
+  ASSERT(lft_index(1, 1) == 5);
+  ASSERT(lft_index(2, 0) == 3);
+  ASSERT(lft_index(2, 1) == 11);
+  ASSERT(lft_index(3, 0) == 7);
+  ASSERT(lft_index(3, 1) == 23);
 }
 
 void test_depth()
 {
-  assert(lft_depth(0) == 0, "depth");
-  assert(lft_depth(2) == 0, "depth");
-  assert(lft_depth(4) == 0, "depth");
-  assert(lft_depth(444) == 0, "depth");
-  assert(lft_depth(1) == 1, "depth");
-  assert(lft_depth(5) == 1, "depth");
-  assert(lft_depth(9) == 1, "depth");
-  assert(lft_depth(13) == 1, "depth");
-  assert(lft_depth(3) == 2, "depth");
-  assert(lft_depth(7) == 3, "depth");
+  ASSERT(lft_depth(0) == 0);
+  ASSERT(lft_depth(2) == 0);
+  ASSERT(lft_depth(4) == 0);
+  ASSERT(lft_depth(444) == 0);
+  ASSERT(lft_depth(1) == 1);
+  ASSERT(lft_depth(5) == 1);
+  ASSERT(lft_depth(9) == 1);
+  ASSERT(lft_depth(13) == 1);
+  ASSERT(lft_depth(3) == 2);
+  ASSERT(lft_depth(7) == 3);
 }
 
 int main(int argc, char** argv)
 {
-  test_index();
-  test_depth();
+  TEST(test_index);
+  TEST(test_depth);
 
-  return 0;
+  return TEST_RESULT;
 }
