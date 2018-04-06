061f7e2c07471eef2f3c9625cd213a5462bcf25c
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-04-06 13:30:21 +0200

    add comments on non ported functions

--- libflattree.h
+++ libflattree.h
@@ -13,4 +13,18 @@ size_t lft_right_child(size_t index);
 size_t lft_left_span(size_t index);
 size_t lft_right_span(size_t index);
 
+// NOTE!
+// The following functions have not been ported from js/rust because
+// it's currently unclear if they are needed at all
+
+// count(index, depth)
+// spans(index, depth)
+
+// The following function has not been ported because afaik it's only
+// used in `print-flat-tree` functionality, so that functionality should
+// probably be moved up a level (basically just doing left_child() /
+// right_child())
+
+// children(index, depth)
+
 #endif // __libflattree_h__
